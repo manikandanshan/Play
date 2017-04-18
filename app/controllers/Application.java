a08765ac72120856679200ea6ae80d9eb98011e6
manikandanshan <manikbhuvan1985@gmail.com>
2017-04-18 18:01:59 +0530

    committed

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("dfzdsadsests."));
+        return ok(index.render("Hello Welcome manikandan."));
     }
 
 }
